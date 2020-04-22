7e785d3ddcb3cbbfdfd32f938cfd77894e1e1901
mayan <mshoshan@uci.edu>
2020-04-21 23:26:32 -0700

    added menu scene and scene loading , and exit

--- Falling with Style VR/Assets/scripts/ui/NextLevel.cs
+++ Falling with Style VR/Assets/scripts/ui/NextLevel.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 public class NextLevel : MonoBehaviour
 {
     public void Next() {
-        if(SceneManager.GetActiveScene().buildIndex<5)
+        if(SceneManager.GetActiveScene().buildIndex<=5)
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
         else
             SceneManager.LoadScene(0);
