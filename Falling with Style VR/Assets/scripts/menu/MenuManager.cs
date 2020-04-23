54925ff0a4fb100d61a81c985d66a7bcce7b2c7e
mayan <mshoshan@uci.edu>
2020-04-22 19:33:57 -0700

    scoreboard added

--- Falling with Style VR/Assets/scripts/menu/MenuManager.cs
+++ Falling with Style VR/Assets/scripts/menu/MenuManager.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
 
     public GameObject[] levels;
+    public Text scoreBoard;
     private void Awake()
     {
         for (int i = 1; i < levels.Length; i++) {
@@ -13,6 +15,12 @@ public class MenuManager : MonoBehaviour
                 levels[i].SetActive(false);
             else
                 Debug.Log(i);
+        }
+
+        scoreBoard.text = "";
+        for (int i = 0; i<GameManager.levelChart.Length; i++) {
+            if (GameManager.levelChart[i] > 0)
+                scoreBoard.text += "Level " + (i + 1).ToString() + ": " + GameManager.levelChart[i].ToString() + "\n";
         }
     }
 
