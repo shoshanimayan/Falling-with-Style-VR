d62d719480cf9afd84a027901e5dc860da3dce61
shoshanimayan <mshoshan@uci.edu>
2021-05-02 20:19:17 -0700

    fix menu

--- Falling with Style VR/Assets/scripts/gameplay/GameManager.cs
+++ Falling with Style VR/Assets/scripts/gameplay/GameManager.cs
@@ -6,7 +6,7 @@ public class GameManager : MonoBehaviour
 {
     public static bool play = false; // is level started
     public static int score=0; //current score
-    public static int[] levelChart = { 0, 0, 0, 0, 0, 0 }; //track scores for each level, need to either expand the array or use alt data struct in future
+    public static int[] levelChart = { 0, 0, 0, 0, 0, 0,0,0,0 }; //track scores for each level, need to either expand the array or use alt data struct in future
     
 
     // Start is called before the first frame update
--- Falling with Style VR/Assets/scripts/ui/NextLevel.cs
+++ Falling with Style VR/Assets/scripts/ui/NextLevel.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 public class NextLevel : MonoBehaviour
 {//TODO:refactor to be in game manager
     public void Next() {
-        if(SceneManager.GetActiveScene().buildIndex<=5)
+        if(SceneManager.GetActiveScene().buildIndex<=8)
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
         else
             SceneManager.LoadScene(0);
